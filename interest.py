e59799c698fd5a3c0c2fe230f81f6856e3b99d9b
gokulavelan <81364052+gokulavelan@users.noreply.github.com>
2021-04-21 10:59:16 +0530

    Add files via upload

---
+++ interest.py
@@ -0,0 +1,6 @@
+num1 = float(input('Enter the principal amount:'))
+yr = float(input('Enter the number of years'))
+inte = float(input('Enter the interest'))
+
+smpint = (num1 * yr * inte)/100
+print(smpint)
