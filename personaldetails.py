d17ff312f14a4caab2cf38cfa31b5e7870876cad
gokulavelan <81364052+gokulavelan@users.noreply.github.com>
2021-04-21 11:14:38 +0530

    Add files via upload

---
+++ personaldetails.py
@@ -0,0 +1,9 @@
+name = input('Enter the name')
+age = input('Enter your age')
+address = input('Enter your address')
+mobno = input('ENter your mobile no')
+
+print('name: ',name)
+print('age: ',age)
+print('address: ',address)
+print('mobile no: ',mobno)
